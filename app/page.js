f6c7d02a2695ef59ebac726a43cfe8c65f926419
Md. Nahid Hasan Bappi <nahidbappi13@gmail.com>
2025-06-23 23:50:16 +0600

    fixed problem: created like button file inside app folder and moved some part of code from page.js to the like button file and treat it as a client side with use client at top

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
-import { useState } from 'react';
- 
+import LikeButton from "./like-button";
+
+
 function Header({ title }) {
   return <h1>{title ? title : 'Default title'}</h1>;
 }
@@ -7,12 +8,7 @@ function Header({ title }) {
 export default function HomePage() {
   const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
  
-  const [likes, setLikes] = useState(0);
- 
-  function handleClick() {
-    setLikes(likes + 1);
-  }
- 
+
   return (
     <div>
       <Header title="Develop. Preview. Ship." />
@@ -21,8 +17,9 @@ export default function HomePage() {
           <li key={name}>{name}</li>
         ))}
       </ul>
+      <LikeButton/>
  
-      <button onClick={handleClick}>Like ({likes})</button>
+      
     </div>
   );
 }
